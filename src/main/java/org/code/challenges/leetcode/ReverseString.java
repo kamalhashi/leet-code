b1eeec95bd7f1a8c65600e70dbd093e4b2c24d7f
kweheliye1 <93469212+kweheliye1@users.noreply.github.com>
2023-07-12 14:45:10 +0400

    solved ReverseString challenge

---
+++ src/main/java/org/code/challenges/leetcode/ReverseString.java
@@ -0,0 +1,24 @@
+package org.code.challenges.leetcode;
+
+
+public class ReverseString {
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] ch = {'a', 'b', 'c'};
+        ReverseString.reverseString(ch);
+        System.out.println(ch);
+    }
+}
