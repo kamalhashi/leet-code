74cffc56b69d17bee8c055156a4758e78ca4b2ba
Kamal Hashi <kamalweheliye@gmail.com>
2023-07-21 10:54:12 +0400

    1413. Minimum Value to Get Positive Step by Step Sum

---
+++ src/main/java/org/code/challenges/leetcode/arrays/medium/prefixsum/MinimumValueToGetPositiveStepByStepSum.java
@@ -0,0 +1,19 @@
+package org.code.challenges.leetcode.arrays.medium.prefixsum;
+
+//Minimum Value to Get Positive Step by Step Sum
+public class MinimumValueToGetPositiveStepByStepSum {
+    public static int minStartValue_PrefixSolution(int[] nums) {
+        int sum = 0;
+        int minPrefixSum = 0;
+        for (int number : nums) {
+            sum += number;
+            minPrefixSum = Math.min(minPrefixSum, sum);
+        }
+        return -minPrefixSum + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minStartValue_PrefixSolution(new int[]{-3, 2, -3, 4, 2}));
+    }
+}
+
