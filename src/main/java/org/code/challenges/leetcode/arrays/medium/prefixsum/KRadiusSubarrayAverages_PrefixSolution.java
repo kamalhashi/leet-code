07d9961f7e0d3767a574fbf468ccfd688aa56973
Kamal Hashi <kamalweheliye@gmail.com>
2023-07-23 13:57:58 +0400

    added new way to solve it

--- src/main/java/org/code/challenges/leetcode/arrays/medium/prefixsum/KRadiusSubarrayAverages_PrefixSolution.java
+++ src/main/java/org/code/challenges/leetcode/arrays/medium/prefixsum/KRadiusSubarrayAverages_PrefixSolution.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class KRadiusSubarrayAverages_PrefixSolution {
 
+    // prefixSum array of size (n+1)
     public static int[] getAverages(int[] nums, int k) {
         if (k == 0) {
             return nums;
@@ -30,7 +31,40 @@ public class KRadiusSubarrayAverages_PrefixSolution {
         return averages;
     }
 
+    public static int[] getAverages2(int[] nums, int k) {
+        if (k == 0) {
+            return nums;
+        }
+        int n = nums.length;
+        int[] averages = new int[n];
+        Arrays.fill(averages, -1);
+
+        if (2 * k + 1 > n) {
+            return averages;
+        }
+
+        long[] prefixSum = new long[n];
+        prefixSum[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        }
+
+        for (int i = k; i < (n - k); i++) {
+            int leftBound = i - k;
+            int rightBound = i + k;
+            long subArraySum = prefixSum[rightBound];
+
+            if (leftBound > 0)
+                subArraySum -= prefixSum[leftBound - 1];
+
+            int average = (int) (subArraySum / (2 * k + 1));
+            averages[i] = average;
+        }
+        return averages;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(getAverages(new int[]{7, 4, 3, 9, 1, 8, 5, 2, 6}, 3)));
+        System.out.println(Arrays.toString(getAverages2(new int[]{2, 1, 3, 4, 5, 8, 1}, 2)));
     }
 }
