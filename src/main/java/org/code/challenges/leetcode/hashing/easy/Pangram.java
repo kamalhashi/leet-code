4a46c82f93a5aff12fb9be60c80cc354d4e298ad
Kamal Hashi <kamalweheliye@gmail.com>
2023-07-30 17:41:16 +0400

    added Pangram check

---
+++ src/main/java/org/code/challenges/leetcode/hashing/easy/Pangram.java
@@ -0,0 +1,31 @@
+package org.code.challenges.leetcode.hashing.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+public class Pangram {
+    public boolean checkIfPangram_FindLettersOneByOne(String sentence) {
+        for (int i = 0; i < 26; i++) {
+            char currChar = (char) ('a' + 1);
+            if (sentence.indexOf(currChar) == -1)
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean checkIfPangram_Set(String sentence) {
+        Set<Character> seen = new HashSet<>();
+        for (char currChar : sentence.toCharArray()) {
+            seen.add(currChar);
+        }
+        return seen.size() == 26;
+    }
+
+
+    public static void main (String[] args){
+        System.out.println(checkIfPangram_Set("thequickbrownfoxjumpsoverthelazydog"));
+        System.out.println(checkIfPangram_Set("leetcode"));
+
+    }
+}
