2a32ff2444f303068bb8042c01e9e30ace8bd114
Kamal Hashi <kamalweheliye@gmail.com>
2024-02-19 14:55:54 +0400

    conflict

---
+++ src/main/java/org/code/challenges/leetcode/topInterview150/easy/LongestCommonPrefix.java
@@ -0,0 +1,30 @@
+package org.code.challenges.leetcode.topInterview150.easy;
+
+public class LongestCommonPrefix {
+
+    //Horizantal
+    public String solutionOne(String[] strs) {
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+            if (prefix.isEmpty()) {
+                return "";
+            }
+        }
+        return prefix;
+    }
+
+    public String solution2(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+
+        return "";
+    }
+}
